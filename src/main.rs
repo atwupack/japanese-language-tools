2c1380954668ffd07dce9aeb50453457482b27a5
André Twupack <atwupack@mailbox.org>
2019-09-13 22:11:18 +0200

    some minor changes

--- src/main.rs
+++ src/main.rs
@@ -11,28 +11,32 @@ fn main() {
         .version("1.0")
         .author("André Twupack")
         .subcommand(SubCommand::with_name("tatoeba-filter")
+            .about("Filter a Tatoeba sentences file by language code.")
             .arg(Arg::with_name("input")
                 .short("i")
                 .takes_value(true)
+                .required(true)
             )
             .arg(Arg::with_name("output")
                 .short("o")
                 .takes_value(true)
+                .required(true)
             )
             .arg(Arg::with_name("language")
                 .short("l")
                 .takes_value(true)
+                .required(true)
             )
         )
         .get_matches();
 
-    if let Some(matches) = matches.subcommand_matches("tatoeba-filter") {
+    if let Some(matches) = matches.subcommand_matches("tatoeba-áfilter") {
         let input = matches.value_of("input").unwrap();
         let output = matches.value_of("output").unwrap();
         let language = matches.value_of("language").unwrap();
 
         if let Err(err) = filter_language(input, output, language) {
-            println!("error running example: {}", err);
+            println!("error running filter: {}", err);
             process::exit(1);
         }
     }
