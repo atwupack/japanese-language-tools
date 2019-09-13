d0ed5f15a5a771aad695d6d34aab8eb90189c63a
André Twupack <atwupack@mailbox.org>
2019-09-13 15:40:33 +0200

    parse Tatoebe csv file with sentences

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::error::Error;
+use std::process;
+
+use serde::Deserialize;
+
+use isolang::Language;
+
+#[derive(Debug, Deserialize)]
+struct Sentence {
+    tatoeba_id: u32,
+    language: Language,
+    text: String,
+}
+
+fn parse_tatoeba() -> Result<(), Box<dyn Error>> {
+    let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .delimiter(b'\t')
+        .from_path("data/sentences.csv")?;
+    for result in rdr.deserialize() {
+        // Notice that we need to provide a type hint for automatic
+        // deserialization.
+        let sentence: Sentence = result?;
+        println!("{:?}", sentence);
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = parse_tatoeba() {
+        println!("error running example: {}", err);
+        process::exit(1);
+    }
 }
